91bf8d735af0620ccee753a582a4ee374a40f9cd
Gautam <72904423+Gautam1201@users.noreply.github.com>
2021-04-20 14:25:16 +0530

    Second Commit

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){
   $(".query").on("click",function(){
-    $(this).toggleClass("afterClick");
+    $(".query").removeClass("afterClick");
+    
+    $(".answered").slideUp();
+    $(".arrow").css({'transform': 'rotate(0deg)'});
     if($(this).next(".answered").css("display") === "none"){
+      $(this).addClass("afterClick");
       $(this).next(".answered").slideDown();
       $(this).children('.arrow').css({'transform': 'rotate(180deg)'});
-
     }
     else{
         $(this).next(".answered").slideUp();
         $(this).children('.arrow').css({'transform': 'rotate(0deg)'});
+        $(this).removeClass("afterClick");
     }
   })
 });
